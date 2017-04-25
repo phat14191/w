0c8dd2f3304b5b0f17e4a13fbe4dd7325469cf10
halle tempest and phat nguyen <halle@>
2017-04-25 10:06:53 -0700

    Saving hide/show second image.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+  $(".clickable").click(function() {
+    $("#is").fadeToggle();
+    $("#ih").fadeToggle();
+  });
+});
+
+$(function() {
+  $(".clickable2").click(function() {
+    $("#as").fadeToggle();
+    $("#ah").fadeToggle();
+  });
+});
